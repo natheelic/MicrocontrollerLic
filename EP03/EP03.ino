416ab90af1dcf3f850b9e3f3fac38e67d715be82
Nathee Srina <nathee.sri@lic.ac.th>
2023-11-30 10:37:21 +0700

    Update EP03.ino

--- EP03/EP03.ino
+++ EP03/EP03.ino
@@ -1,9 +1,10 @@
 
 // Define the pins for the switches
-byte switchPins[] = {2, 3, 4};
+byte switchPins[] = {13, 12, 14};
 
 // Define the pins for the LEDs
-byte ledPins[] = {5, 6, 7, 8, 9, 10, 11, 12};
+// byte ledPins[] = {5, 6, 7, 8, 9, 10, 11, 12};
+byte ledPins[] = {15, 2, 4, 16, 17, 5, 18, 19};
 
 void setup()
 {
@@ -13,59 +14,41 @@ void setup()
     {
         pinMode(switchPins[i], INPUT_PULLUP);
         Serial.print("Switch ");
-        Serial.print(i + 1);
+        Serial.println(i + 1);
     }
     // Set the LED pins as outputs
     for (int i = 0; i < 8; i++)
     {
         pinMode(ledPins[i], OUTPUT);
         Serial.print("LED ");
-        Serial.print(i + 1);
+        Serial.println(i + 1);
     }
 }
 
 void loop()
 {
-    // // Read the state of the switches
-    // int switch1State = digitalRead(switch1Pin);
-    // int switch2State = digitalRead(switch2Pin);
-    // int switch3State = digitalRead(switch3Pin);
+    // Read the state of the switches
+    int switch1State = digitalRead(switchPins[0]);
+    int switch2State = digitalRead(switchPins[1]);
+    int switch3State = digitalRead(switchPins[2]);
 
-    // // Control the LEDs based on the switch states
-    // if (switch1State == HIGH)
-    // {
-    //     digitalWrite(led1Pin, HIGH);
-    //     digitalWrite(led2Pin, HIGH);
-    //     digitalWrite(led3Pin, HIGH);
-    // }
-    // else
-    // {
-    //     digitalWrite(led1Pin, LOW);
-    //     digitalWrite(led2Pin, LOW);
-    //     digitalWrite(led3Pin, LOW);
-    // }
-
-    // if (switch2State == HIGH)
-    // {
-    //     digitalWrite(led4Pin, HIGH);
-    //     digitalWrite(led5Pin, HIGH);
-    //     digitalWrite(led6Pin, HIGH);
-    // }
-    // else
-    // {
-    //     digitalWrite(led4Pin, LOW);
-    //     digitalWrite(led5Pin, LOW);
-    //     digitalWrite(led6Pin, LOW);
-    // }
-
-    // if (switch3State == HIGH)
-    // {
-    //     digitalWrite(led7Pin, HIGH);
-    //     digitalWrite(led8Pin, HIGH);
-    // }
-    // else
-    // {
-    //     digitalWrite(led7Pin, LOW);
-    //     digitalWrite(led8Pin, LOW);
-    // }
+    // Control the LEDs based on the switch states
+    if (switch1State == LOW)
+    {
+        digitalWrite(ledPins[0], HIGH);
+        digitalWrite(ledPins[1], HIGH);
+        digitalWrite(ledPins[2], HIGH);
+        // for(int i = 0; i < 3; i++){
+        //     digitalWrite(ledPins[i], HIGH);
+        // }
+    }
+    else
+    {
+        digitalWrite(ledPins[0], LOW);
+        digitalWrite(ledPins[1], LOW);
+        digitalWrite(ledPins[2], LOW);
+        // for(int i = 0; i < 3; i++){
+        //     digitalWrite(ledPins[i], LOW);
+        // }
+    }
 }
