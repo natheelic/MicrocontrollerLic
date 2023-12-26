9bfd4cffc40a22f5d97b67e4c172d31b9b624a6a
Nathee Srina <s_nathee@live.com>
2023-12-26 14:55:49 +0700

    Update WEMOS_LOLIN32_DEMOTEXT_WIFI.ino

--- WEMOS_LOLIN32_DEMOTEXT_WIFI/WEMOS_LOLIN32_DEMOTEXT_WIFI.ino
+++ WEMOS_LOLIN32_DEMOTEXT_WIFI/WEMOS_LOLIN32_DEMOTEXT_WIFI.ino
@@ -1,14 +1,31 @@
-#include <WiFiManager.h>  // https://github.com/tzapu/WiFiManager
+/**
+ * WiFiManager advanced demo, contains advanced configurartion options
+ * Implements TRIGGEN_PIN button press, press for ondemand configportal, hold for 3 seconds for reset settings.
+ */
 // OLED
 #include <Wire.h>         // Only needed for Arduino 1.6.5 and earlier
 #include "SSD1306Wire.h"  // legacy: #include "SSD1306.h"
+
+#include <WiFiManager.h>  // https://github.com/tzapu/WiFiManager
 
 #define TRIGGER_PIN 0
 
 SSD1306Wire display(0x3c, 4, 15);  // กำหนดตำแหน่ง address และขา SDA , SCL ของจอ OlED
 String msg = "";
 
+// wifimanager can run in a blocking mode or a non blocking mode
+// Be sure to know how to process loops with no delay() if using non blocking
+bool wm_nonblocking = false;  // change to true to use non blocking
+
+WiFiManager wm;                     // global wm instance
+WiFiManagerParameter custom_field;  // global param ( for non blocking w params )
+
 void setup() {
+  WiFi.mode(WIFI_STA);  // explicitly set mode, esp defaults to STA+AP
+  Serial.begin(115200);
+  Serial.setDebugOutput(true);
+  delay(3000);
+  Serial.println("\n Starting");
 
   pinMode(16, OUTPUT);
   digitalWrite(16, 1);  // กำหนดขา Reset ของ OLED
@@ -17,26 +34,31 @@ void setup() {
   display.init();
 
   display.flipScreenVertically();
-  display.setFont(ArialMT_Plain_10);
-
-  WiFi.mode(WIFI_STA);  // explicitly set mode, esp defaults to STA+AP
-  Serial.begin(115200);
-  Serial.setDebugOutput(true);
-  delay(3000);
-  Serial.println("\n Starting");
 
   display.setTextAlignment(TEXT_ALIGN_LEFT);
   display.setFont(ArialMT_Plain_16);
   msg = "Starting";
   display.drawString(0, 0, msg);
   display.display();
+  delay(2000);
+  display.setFont(ArialMT_Plain_10);
+  display.drawString(0, 17, "Waiting for Connect.");
+  display.display();
 
   pinMode(TRIGGER_PIN, INPUT);
+
+  // wm.resetSettings(); // wipe settings
 
   if (wm_nonblocking) wm.setConfigPortalBlocking(false);
 
   // add a custom input field
   int customFieldLength = 40;
+
+
+  // new (&custom_field) WiFiManagerParameter("customfieldid", "Custom Field Label", "Custom Field Value", customFieldLength,"placeholder=\"Custom Field Placeholder\"");
+
+  // test custom html input type(checkbox)
+  // new (&custom_field) WiFiManagerParameter("customfieldid", "Custom Field Label", "Custom Field Value", customFieldLength,"placeholder=\"Custom Field Placeholder\" type=\"checkbox\""); // custom html type
 
   // test custom html(radio)
   const char* custom_radio_str = "<br/><label for='customfieldid'>Custom Field Label</label><input type='radio' name='customfieldid' value='1' checked> One<br><input type='radio' name='customfieldid' value='2'> Two<br><input type='radio' name='customfieldid' value='3'> Three";
@@ -46,31 +68,62 @@ void setup() {
   wm.setSaveParamsCallback(saveParamCallback);
 
   // custom menu via array or vector
+  //
+  // menu tokens, "wifi","wifinoscan","info","param","close","sep","erase","restart","exit" (sep is seperator) (if param is in menu, params will not show up in wifi page!)
+  // const char* menu[] = {"wifi","info","param","sep","restart","exit"};
+  // wm.setMenu(menu,6);
   std::vector<const char*> menu = { "wifi", "info", "param", "sep", "restart", "exit" };
   wm.setMenu(menu);
 
   // set dark theme
   wm.setClass("invert");
+
+
+  //set static ip
+  // wm.setSTAStaticIPConfig(IPAddress(10,0,1,99), IPAddress(10,0,1,1), IPAddress(255,255,255,0)); // set static ip,gw,sn
+  // wm.setShowStaticFields(true); // force show static ip fields
+  // wm.setShowDnsFields(true);    // force show dns field always
+
+  // wm.setConnectTimeout(20); // how long to try to connect for before continuing
   wm.setConfigPortalTimeout(30);  // auto close configportal after n seconds
+  // wm.setCaptivePortalEnable(false); // disable captive portal redirection
+  // wm.setAPClientCheck(true); // avoid timeout if client connected to softap
+
+  // wifi scan settings
+  // wm.setRemoveDuplicateAPs(false); // do not remove duplicate ap names (true)
+  // wm.setMinimumSignalQuality(20);  // set min RSSI (percentage) to show in scans, null = 8%
+  // wm.setShowInfoErase(false);      // do not show erase button on info page
+  // wm.setScanDispPerc(true);       // show RSSI as percentage not graph icons
+
+  // wm.setBreakAfterConfig(true);   // always exit configportal even if wifi save fails
 
   bool res;
   // res = wm.autoConnect(); // auto generated AP name from chipid
   // res = wm.autoConnect("AutoConnectAP"); // anonymous ap
-  res = wm.autoConnect("AutoConnectAP", "password");  // password protected ap
+  res = wm.autoConnect("ESP-NOOKNICK", "password");  // password protected ap
+
+
+  display.setTextAlignment(TEXT_ALIGN_LEFT);
+  display.setFont(ArialMT_Plain_10);
+  display.clear();
 
   if (!res) {
     Serial.println("Failed to connect or hit timeout");
-    msg = "Failed to connect or hit timeout";
+    // msg = "Failed to connect , Press Reset Button.";
+    display.drawString(0, 17, "Failed to connect");
+    display.drawString(0, 27, "Press Reset Button.");
+    display.display();
+    while (1)
+      ;
     // ESP.restart();
   } else {
     //if you get here you have connected to the WiFi
     Serial.println("connected...yeey :)");
     msg = "connected...yeey :)";
+    display.drawString(0, 17, msg);
+    display.display();
   }
-  display.setTextAlignment(TEXT_ALIGN_LEFT);
-  display.setFont(ArialMT_Plain_16);
-  display.drawString(0, 15, msg);
-  display.display();
+  delay(3000);
 }
 
 void checkButton() {
@@ -105,6 +158,7 @@ void checkButton() {
   }
 }
 
+
 String getParam(String name) {
   //read parameter from server, for customhmtl input
   String value;
@@ -134,7 +188,7 @@ void drawFontFaceDemo() {
 void loop() {
   if (wm_nonblocking) wm.process();  // avoid delays() in loop when non-blocking and other long running code
   checkButton();
-
+  // put your main code here, to run repeatedly:
   // clear the display
   display.clear();
   // draw the current demo method
