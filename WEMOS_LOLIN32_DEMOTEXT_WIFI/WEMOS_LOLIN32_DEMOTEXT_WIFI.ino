f5d43e805e31bbb69806184b4657e5da24a1a0df
Nathee Srina <95066184+natheelic@users.noreply.github.com>
2023-12-26 20:09:45 +0700

    update line

--- WEMOS_LOLIN32_DEMOTEXT_WIFI/WEMOS_LOLIN32_DEMOTEXT_WIFI.ino
+++ WEMOS_LOLIN32_DEMOTEXT_WIFI/WEMOS_LOLIN32_DEMOTEXT_WIFI.ino
@@ -8,10 +8,25 @@
 
 #include <WiFiManager.h>  // https://github.com/tzapu/WiFiManager
 
+#include <ESP32Ping.h>
+
+#include <TridentTD_LineNotify.h>
+
+#define LINE_TOKEN  ""
+const IPAddress remote_ip();
+// const IPAddress remote_ip();
 #define TRIGGER_PIN 0
 
 SSD1306Wire display(0x3c, 4, 15);  // กำหนดตำแหน่ง address และขา SDA , SCL ของจอ OlED
 String msg = "";
+String chkip = "";
+
+// Generally, you should use "unsigned long" for variables that hold time
+// The value will quickly become too large for an int to store
+unsigned long previousMillis = 0;  // will store last time LED was updated
+unsigned long ttime = 0;
+// constants won't change:
+const long interval = 1800000;  // interval at which to blink (milliseconds)
 
 // wifimanager can run in a blocking mode or a non blocking mode
 // Be sure to know how to process loops with no delay() if using non blocking
@@ -29,7 +44,8 @@ void setup() {
 
   pinMode(16, OUTPUT);
   digitalWrite(16, 1);  // กำหนดขา Reset ของ OLED
-
+  // กำหนด Line Token
+  LINE.setToken(LINE_TOKEN);
   // Initialising the UI will init the display too.
   display.init();
 
@@ -47,59 +63,25 @@ void setup() {
 
   pinMode(TRIGGER_PIN, INPUT);
 
-  // wm.resetSettings(); // wipe settings
-
   if (wm_nonblocking) wm.setConfigPortalBlocking(false);
-
-  // add a custom input field
   int customFieldLength = 40;
 
-
-  // new (&custom_field) WiFiManagerParameter("customfieldid", "Custom Field Label", "Custom Field Value", customFieldLength,"placeholder=\"Custom Field Placeholder\"");
-
-  // test custom html input type(checkbox)
-  // new (&custom_field) WiFiManagerParameter("customfieldid", "Custom Field Label", "Custom Field Value", customFieldLength,"placeholder=\"Custom Field Placeholder\" type=\"checkbox\""); // custom html type
-
-  // test custom html(radio)
   const char* custom_radio_str = "<br/><label for='customfieldid'>Custom Field Label</label><input type='radio' name='customfieldid' value='1' checked> One<br><input type='radio' name='customfieldid' value='2'> Two<br><input type='radio' name='customfieldid' value='3'> Three";
   new (&custom_field) WiFiManagerParameter(custom_radio_str);  // custom html input
 
   wm.addParameter(&custom_field);
   wm.setSaveParamsCallback(saveParamCallback);
 
-  // custom menu via array or vector
-  //
-  // menu tokens, "wifi","wifinoscan","info","param","close","sep","erase","restart","exit" (sep is seperator) (if param is in menu, params will not show up in wifi page!)
-  // const char* menu[] = {"wifi","info","param","sep","restart","exit"};
-  // wm.setMenu(menu,6);
   std::vector<const char*> menu = { "wifi", "info", "param", "sep", "restart", "exit" };
   wm.setMenu(menu);
 
   // set dark theme
   wm.setClass("invert");
 
-
-  //set static ip
-  // wm.setSTAStaticIPConfig(IPAddress(10,0,1,99), IPAddress(10,0,1,1), IPAddress(255,255,255,0)); // set static ip,gw,sn
-  // wm.setShowStaticFields(true); // force show static ip fields
-  // wm.setShowDnsFields(true);    // force show dns field always
-
-  // wm.setConnectTimeout(20); // how long to try to connect for before continuing
-  wm.setConfigPortalTimeout(30);  // auto close configportal after n seconds
-  // wm.setCaptivePortalEnable(false); // disable captive portal redirection
-  // wm.setAPClientCheck(true); // avoid timeout if client connected to softap
-
-  // wifi scan settings
-  // wm.setRemoveDuplicateAPs(false); // do not remove duplicate ap names (true)
-  // wm.setMinimumSignalQuality(20);  // set min RSSI (percentage) to show in scans, null = 8%
-  // wm.setShowInfoErase(false);      // do not show erase button on info page
-  // wm.setScanDispPerc(true);       // show RSSI as percentage not graph icons
-
-  // wm.setBreakAfterConfig(true);   // always exit configportal even if wifi save fails
+  wm.setConfigPortalTimeout(30);
 
   bool res;
-  // res = wm.autoConnect(); // auto generated AP name from chipid
-  // res = wm.autoConnect("AutoConnectAP"); // anonymous ap
+
   res = wm.autoConnect("ESP-NOOKNICK", "password");  // password protected ap
 
 
@@ -123,9 +105,25 @@ void setup() {
     display.drawString(0, 17, msg);
     display.display();
     msg = "IP : " + WiFi.localIP().toString();
+    pingIPadd();
   }
   delay(3000);
 }
+
+void pingIPadd() {
+  Serial.print("Pinging ip ");
+  Serial.println(remote_ip);
+
+  if (Ping.ping(remote_ip)) {
+    Serial.println("Pass!!");
+    chkip = "Pass!!";
+  } else {
+    Serial.println("Error :(");
+    chkip = "Error :(";
+  }
+  LINE.notify(chkip);
+}
+
 
 void checkButton() {
   // check for button press
@@ -181,9 +179,11 @@ void drawFontFaceDemo() {
   display.setFont(ArialMT_Plain_10);
   display.drawString(0, 0, msg);
   display.setFont(ArialMT_Plain_16);
-  display.drawString(0, 10, "Hello ALL");
-  display.setFont(ArialMT_Plain_24);
-  display.drawString(0, 26, "EN 2023");
+  display.drawString(0, 10, chkip);
+  display.setFont(ArialMT_Plain_10);
+  display.drawString(0, 26, "Ping : ");
+  display.setFont(ArialMT_Plain_10);
+  display.drawString(0, 36, "Wait : "+ String(ttime));
 }
 
 void loop() {
@@ -200,4 +200,12 @@ void loop() {
   // write the buffer to the display
   display.display();
   delay(10);
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval) {
+    // save the last time you blinked the LED
+    previousMillis = currentMillis;
+
+    pingIPadd();
+  }
+  ttime =(currentMillis - previousMillis)/1000;
 }
