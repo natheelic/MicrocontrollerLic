9e8653693ddb83947f549fbc3c280a8f95fd2491
Nathee Srina <s_nathee@live.com>
2023-12-26 11:53:40 +0700

    Create LAB-4-25.ino

---
+++ LAB-4-25/LAB-4-25/LAB-4-25.ino
@@ -0,0 +1,27 @@
+// #include <LedControl.h> ยกเลิกใช้
+#include "LedController.hpp"
+
+// LedControl lc = LedControl(18, 23, 5, 1); ยกเลิกใช้
+#define DIN 23
+#define CS 5
+#define CLK 18
+LedController<1, 1> lc;
+
+void setup() {
+  lc = LedController<1, 1>(DIN, CLK, CS);
+  // lc.shutdown(0, false); ยกเลิกใช้
+  // lc.setIntensity(0, 10); ยกเลิกใช้
+  lc.setIntensity(2);
+  // lc.clearDisplay(); ยกเลิกใช้
+  lc.clearMatrix();
+}
+
+void loop() {
+  lc.setRow(0, 7, B01001111);  //E
+  lc.setRow(0, 6, B01110110);  //n
+  lc.setDigit(0,3,2,false);
+  lc.setDigit(0,2,0,false);
+  lc.setDigit(0,1,2,false);
+  lc.setDigit(0,0,3,false);
+  while (1);
+}
